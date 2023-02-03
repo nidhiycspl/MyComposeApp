9cd09d5c48279e1db6dc9ad7ac262e6522953865
nidhiycspl <nidhi@ycspl.in>
2023-02-03 19:01:38 +0530

    dropdown menu

--- app/src/main/java/com/example/mycomposeapp/Home.kt
+++ app/src/main/java/com/example/mycomposeapp/Home.kt
@@ -1,12 +1,15 @@
 package com.example.mycomposeapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -16,20 +19,23 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycomposeapp.model.ListModel
 import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 
 class Home : ComponentActivity() {
-    var inputValue = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Column {
-//                uiContent(inputValue)
-               recycleViewFun()
+                  AddItemToRecycleOnButtonClick(this@Home)
+              //  recycleViewMultipleSelection()
              //   animateVisibility()
             }
         }
@@ -77,7 +83,7 @@ fun animateVisibility(){
 }
 
 @Composable
-fun recycleViewFun(){
+fun recycleViewMultipleSelection(){
     /*LazyColumn{
         itemsIndexed(
             listOf("This","is","Jetpack","Compose")
@@ -98,11 +104,11 @@ fun recycleViewFun(){
         items(items.size){ i->
             RecycleUi(model= items[i]){
                 items = items.mapIndexed { index, item ->
-               if(i == index){
+               if(i == index) {
                    item.copy(isSelected = it)
-               }else{
+               }
+               else {
                    item
-
                }
            }
 
@@ -114,26 +120,98 @@ fun recycleViewFun(){
 }
 
 @Composable
-fun UiContent(inputValue: String) {
+fun AddItemToRecycleOnButtonClick(context : Context) {
+    var list = remember { mutableStateListOf<String>() }
+
+    var inputText by remember {
+        mutableStateOf("")
+    }
+
+
     Column(modifier = Modifier
         .fillMaxWidth()
         .padding(10.dp)) {
-        TextField(value = "dwdwdw",
+        TextField(
+            value = inputText,
             onValueChange = {
-            }
+                inputText = it
+            },
+            placeholder = {Text(text = "Enter Text")}
         )
-        
+
         Spacer(modifier = Modifier.height(10.dp))
 
-        Button(onClick = { },) {
-
+        Button(onClick = {
+            if(inputText.isNotEmpty()){
+                list.add(inputText)
+            }
+            inputText = ""
+        },
+        ) {
+            Text("Add")
         }
+        RecycleList(list)
     }
 
 }
 
 @Composable
-fun RecycleUi(model: ListModel,onItemChange:(Boolean)->Unit ) {
+fun RecycleList(list : List<String>) {
+    var mDisplayMenu by remember { mutableStateOf(false) }
+
+
+    LazyColumn {
+        items(list.size)
+        {index ->
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Red)
+                .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Text( list[index],
+                    fontSize = 16.sp,
+                    style = TextStyle(color = Color.White)
+                )   
+
+                Spacer(modifier = Modifier.weight(.2f))
+
+                Image(painterResource(id = R.drawable.baseline_more_vert_24),
+                contentDescription = null,
+
+                    modifier = Modifier.clickable {
+                    mDisplayMenu = true
+                }
+                )
+
+                DropdownMenu(expanded = mDisplayMenu,
+                    onDismissRequest = {
+                        mDisplayMenu = false
+                    }) {
+                    DropdownMenuItem(onClick = { })
+                    {
+                        Text("Edit")
+                    }
+
+                    DropdownMenuItem(onClick = { })
+                    {
+                        Text("Delete")
+                    }
+                }
+               // OpenDropDown()
+
+            }
+
+        }
+    }
+}
+
+
+@Composable
+fun RecycleUi(model: ListModel,onItemChange:(Boolean) -> Unit ) {
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(10.dp)
@@ -141,15 +219,14 @@ fun RecycleUi(model: ListModel,onItemChange:(Boolean)->Unit ) {
             onItemChange(model.isSelected.not())
         },
     elevation = 5.dp,
-    backgroundColor = Color(0xFFF6F6F6)
-    ) {
-        Row(modifier = Modifier
+    backgroundColor = Color(0xFFF6F6F6)) {
+        Row( modifier = Modifier
             .fillMaxWidth()
             .padding(12.dp),
         horizontalArrangement = Arrangement.SpaceBetween)
         {
             Text(model.title)
-            if(model.isSelected){
+            if(model.isSelected) {
                 Icon(
                     imageVector = Icons.Default.Check ,
                     contentDescription = "Selected",
@@ -158,13 +235,6 @@ fun RecycleUi(model: ListModel,onItemChange:(Boolean)->Unit ) {
             }
         }
     }
-}
-
-
-
-@Composable
-fun HomeDrawer(){
-
 }
 
 @Composable
@@ -176,6 +246,6 @@ fun Greeting3(name: String) {
 @Composable
 fun DefaultPreview3() {
     MyComposeAppTheme {
-        HomeDrawer()
+
     }
 }
